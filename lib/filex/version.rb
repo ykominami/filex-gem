fdf316ec49fa8797ff0e7bb733875428bab61934
yasuo kominami <ykominami@gmail.com>
2019-04-25 16:50:07 +0900

    initial commit

---
+++ lib/filex/version.rb
@@ -0,0 +1,3 @@
+module Filex
+  VERSION = "0.1.0"
+end
---
+++ spec/filex_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Filex do
+  it "has a version number" do
+    expect(Filex::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
