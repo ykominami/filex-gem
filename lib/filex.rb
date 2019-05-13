5ac9d9b9d9fa2181504899c18896f440cfba0558
yasuo kominami <ykominami@gmail.com>
2019-05-13 21:20:54 +0900

    call methods of Messagex class correctlly

--- lib/filex.rb
+++ lib/filex.rb
@@ -11,18 +11,18 @@ module Filex
 
   class Filex
     def self.setup(mes)
-      mes.addExitCode("EXIT_CODE_CANNOT_ANALYZE_YAMLFILE")
-      mes.addExitCode("EXIT_CODE_NAME_ERROR_EXCEPTION_IN_ERUBY")
-      mes.addExitCode("EXIT_CODE_ERROR_EXCEPTION_IN_ERUBY")
+      mes.add_exitcode("EXIT_CODE_CANNOT_ANALYZE_YAMLFILE")
+      mes.add_exitcode("EXIT_CODE_NAME_ERROR_EXCEPTION_IN_ERUBY")
+      mes.add_exitcode("EXIT_CODE_ERROR_EXCEPTION_IN_ERUBY")
     end
 
     def self.load_yaml(str, mes)
       yamlhs = {}
       begin
-        yamlhs = YAML.safe_load(str)
+        yamlhs = YAML.safe_load(str, [Date, Symbol])
       rescue Error => e
-        mes.outputException(e)
-        mes.outputFatal("str=#{str}")
+        mes.output_exception(e)
+        mes.output_fatal("str=#{str}")
         exit(mes.ec("EXIT_CODE_CANNOT_ANALYZE_YAMLFILE"))
       end
 
@@ -37,8 +37,8 @@ module Filex
     def self.check_and_expand_yamlfile(yamlfname, objx, mes)
       lines = Filex.check_and_expand_file_lines(yamlfname, objx, mes)
       str = escape_by_single_quote_with_lines_in_yamlformat(lines, mes).join("\n")
-      mes.outputDebug("=str")
-      mes.outputDebug(str)
+      mes.output_debug("=str")
+      mes.output_debug(str)
       load_yaml(str, mes)
     end
 
@@ -53,28 +53,28 @@ module Filex
           strdata = File.read(fname)
         rescue IOError => e
           mesg = "Can't read #{fname}"
-          mes.outputFatal(mesg)
-          mes.outputException(e)
+          mes.output_fatal(mesg)
+          mes.output_exception(e)
           exit(mes.ec("EXIT_CODE_CANNOT_READ_FILE"))
         rescue SystemCallError => e
           mesg = "Can't write #{fname}"
-          mes.outputFatal(mesg)
-          mes.outputException(e)
+          mes.output_fatal(mesg)
+          mes.output_exception(e)
           exit(mes.ec("EXIT_CODE_CANNOT_READ_FILE"))
         end
       else
         mesg = %Q(Can not find #{fname} or is empty| size=|#{size}|)
-        mes.outputError(mesg)
+        mes.output_error(mesg)
         exit(mes.ec("EXIT_CODE_CANNOT_FIND_FILE_OR_EMPTY"))
       end
 
       if strdata.strip.empty?
         mesg = %Q(#{fname} is empty)
-        mes.outputFatal(mesg)
+        mes.output_fatal(mesg)
         exit(mes.ec("EXIT_CODE_FILE_IS_EMPTY"))
       else
         digest = Digest::MD5.hexdigest(strdata)
-        mes.outputInfo(digest)
+        mes.output_info(digest)
       end
 
       strdata
@@ -82,16 +82,16 @@ module Filex
 
     def self.expand_str(eruby_str, data, mes, fnames={})
       begin
-        mes.outputInfo("eruby_str=|#{eruby_str}|")
-        mes.outputInfo("data=#{data}")
+        mes.output_info("eruby_str=|#{eruby_str}|")
+        mes.output_info("data=#{data}")
         strdata = Erubis::Eruby.new(eruby_str).result(data)
       rescue NameError => e
-        mes.outputException(e)
-        fnames.map {|x| mes.outputFatal(%Q(#{x[0]}=#{x[1]})) }
+        mes.output_exception(e)
+        fnames.map {|x| mes.output_fatal(%Q(#{x[0]}=#{x[1]})) }
         exit(mes.ec("EXIT_CODE_NAME_ERROR_EXCEPTION_IN_ERUBY"))
       rescue Error => e
-        mes.outputException(e)
-        fnames.map {|x| mes.outputFatal(%Q(#{x[0]}=#{x[1]})) }
+        mes.output_exception(e)
+        fnames.map {|x| mes.output_fatal(%Q(#{x[0]}=#{x[1]})) }
         exit(mes.ec("EXIT_CODE_ERROR_EXCEPTION_IN_ERUBY"))
       end
       strdata
@@ -99,9 +99,9 @@ module Filex
 
     def self.check_and_expand_file(fname, objx, mes)
       strdata = check_and_load_file(fname, mes)
-      mes.outputInfo("fname=#{fname}")
-      mes.outputInfo("strdata=#{strdata}")
-      mes.outputInfo("objx=#{objx}")
+      mes.output_info("fname=#{fname}")
+      mes.output_info("strdata=#{strdata}")
+      mes.output_info("objx=#{objx}")
       strdata2 = expand_str(strdata, objx, mes, { fname: fname })
       strdata2
     end
@@ -144,16 +144,16 @@ module Filex
 
     def self.escape_single_quote_yaml_first(line, state)
       k, v = colon_space(line)
-      state[:mes].outputInfo("1|k=#{k}")
+      state[:mes].output_info("1|k=#{k}")
       k, v = colon(line) unless k
-      state[:mes].outputInfo("2|k=#{k}")
+      state[:mes].output_info("2|k=#{k}")
 
       if v&.index("-")
         k, v = hyphen_space(line)
-        state[:mes].outputInfo("3|k=#{k}")
+        state[:mes].output_info("3|k=#{k}")
         if k
           state[:need_quoto] = true
-          state[:mes].outputInfo("NQ|1|need_quoto=#{state[:need_quoto]}")
+          state[:mes].output_info("NQ|1|need_quoto=#{state[:need_quoto]}")
         end
       end
 
@@ -165,7 +165,7 @@ module Filex
     end
 
     def self.escape_single_quote_yaml_second(line, state, key, value)
-      state[:mes].outputInfo("4|k=#{key}|v=#{value}")
+      state[:mes].output_info("4|k=#{key}|v=#{value}")
 
       return [key, value] if value.nil? || value.strip.empty?
 
@@ -176,16 +176,16 @@ module Filex
       return([key, value]) if /\d:/.match?(value)
 
       k2, v2 = colon_space(value)
-      state[:mes].outputInfo("51|k2=#{k2}|v2=#{v2}")
+      state[:mes].output_info("51|k2=#{k2}|v2=#{v2}")
 
       unless k2
         k3, v3 = colon_not_space(value)
-        state[:mes].outputInfo("52|k3=#{k3}|v3=#{v3}")
+        state[:mes].output_info("52|k3=#{k3}|v3=#{v3}")
         state[:need_quoto] = true
 
         unless k3
           k2, v2 = colon(value)
-          state[:mes].outputInfo("53|k2=#{k2}|v2=#{v2}")
+          state[:mes].output_info("53|k2=#{k2}|v2=#{v2}")
           state[:need_quoto] = true
         end
       end
@@ -193,7 +193,7 @@ module Filex
       if k2
         key += k2
         value = v2
-        state[:mes].outputInfo("6|k=#{key}|v=#{value}")
+        state[:mes].output_info("6|k=#{key}|v=#{value}")
       end
       [key, value]
     end
@@ -203,17 +203,17 @@ module Filex
 
       unless state[:need_quoto]
         if value.index(":") || value.index("*")
-          state[:mes].outputInfo("1 not need_quoto")
+          state[:mes].output_info("1 not need_quoto")
           unless value.index("-")
             state[:need_quoto] = true
-            state[:mes].outputInfo("NQ|2|need_quoto=#{state[:need_quoto]}")
+            state[:mes].output_info("NQ|2|need_quoto=#{state[:need_quoto]}")
           end
-          state[:mes].outputInfo("1A need_quoto=#{state[:need_quoto]}")
+          state[:mes].output_info("1A need_quoto=#{state[:need_quoto]}")
         end
       end
 
       if state[:need_quoto] && !state[:has_quoto]
-        state[:mes].outputInfo("2 need_quoto")
+        state[:mes].output_info("2 need_quoto")
         key + %q(') + value + %q(')
       else
         line
